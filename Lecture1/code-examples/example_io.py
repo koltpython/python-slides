3a7a099fcec76644fde0b58cd02311a963610057
Ahmet Uysal <auysal16@ku.edu.tr>
2019-02-18 00:23:35 +0300

    Updated final slides for lecture 1

---
+++ Lecture1/code-examples/example_io.py
@@ -0,0 +1,5 @@
+number = input('Please enter a number:')
+# Assume user entered 34
+result = number * 2
+# What will we see in console?
+print(result)
--- Lecture1/code-examples/io.py
+++ Lecture1/code-examples/io.py
@@ -1,4 +1,5 @@
-# Print descriptive text to console and assign input to variable
+# Print descriptive text to console
+# and assign input to variable
 name = input('Enter a sentence:')
 # Greet user
 print('Hello from Python,', name) 
