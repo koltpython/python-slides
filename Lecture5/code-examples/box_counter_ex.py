c9f431449412882b461db74a001d6e1eae4e28a0
Ahmet Uysal <auysal16@ku.edu.tr>
2019-03-17 17:31:20 +0300

    Added first half of lecture5 sildes

---
+++ Lecture5/code-examples/box_counter_ex.py
@@ -0,0 +1,9 @@
+my_fav_number = 13
+other_number = my_fav_number
+other_number += 3
+print(my_fav_number)  # => 13
+
+my_secret_box = [0, 1, 2]
+other_box = my_secret_box
+other_box.remove(2)
+print(my_secret_box)  # => [0, 1] 
---
+++ Lecture5/code-examples/identity.py
@@ -0,0 +1,4 @@
+a = 1000
+b = 1000
+a == b  # => True
+a is b  # => False
