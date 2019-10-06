14c4ba3b5985d00224f312b7f09f60ec595c790b
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-06 18:34:43 +0300

    Started changing lecture3 slides

---
+++ Lecture3/code-examples/branching_example.py
@@ -0,0 +1,9 @@
+if have_ideas_for_exciting_examples():
+    .
+    .
+    .
+elif gul_sena_has_awesome_ideas():
+    let_her_prepare_the_slides()
+else:
+    prepare_slides_about_your_misery()
+    apologize_to_class()
---
+++ Lecture3/code-examples/string_operations.py
@@ -0,0 +1,8 @@
+print('This a simple calculator program.')
+number1 = input('Please enter the first number:')
+number2 = input('Please enter the second number:')
+print(f'{number1}+{number2} is {number1 + number2}')
+
+number1 = int(input('First number:'))
+number2 = input('Please enter the second number:')
+print(f'{number1}x{number2} is {number1 * number2}')
