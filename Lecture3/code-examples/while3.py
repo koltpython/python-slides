73b5eff85d8c3b9d6c07ea5e9604d773d8b51f33
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-06 20:52:20 +0300

    Added lecture3 while loops section

--- Lecture3/code-examples/while3.py
+++ Lecture3/code-examples/while3.py
@@ -1,6 +1,5 @@
-x = 10
-a_list = [1, 2, 3, 4, 5, 6, 7]
-
-while len(a_list) < x:
-    a_list.append(0)
-    print(a_list)
+counter = 11
+while counter > 6:
+    counter -= 1
+    print(2**counter)
+    counter -= 1
