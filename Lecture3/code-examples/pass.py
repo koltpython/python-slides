c43cb1ab8802407eb7ac5e69cada22deaea70e4e
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-06 23:22:14 +0300

    Added final version of lecture3 slide

--- Lecture3/code-examples/pass.py
+++ Lecture3/code-examples/pass.py
@@ -1,6 +1,5 @@
 for letter in 'Python': 
    if letter == 'y':
       pass
-      print ('In pass case')
    print(letter)
 
--- Lecture3/code-examples/range.py
+++ Lecture3/code-examples/range.py
@@ -2,7 +2,6 @@ a = range(3) # => generates 0, 1, 2
 b = range(0,3) # => generates 0, 1, 2
 c = range(2,4) # => generates 2, 3
 d = range(0,6,2) # => generates 0, 2, 4
-
 0 in a # => True
 1 in b # => True
 4 in c # => False
