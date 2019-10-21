7b084eec7040f2bd44548de40b47d98c89800aba
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-21 10:23:35 +0300

    Divided Functions Section to Subsections

--- Lecture5/code-examples/valid2.py
+++ Lecture5/code-examples/valid2.py
@@ -1,6 +1,6 @@
 # required argument missing
 info()
-# non-keyword argument after a keyword argument
+# non-keyword argument after a keyword arg
 info(num=2, 'Jane')
 # duplicate value for the same argument
 info(2, num=3)
