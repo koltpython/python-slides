9e69abe43c4270fbd48edd571032b53281291343
Ahmet Uysal <auysal16@ku.edu.tr>
2019-11-11 02:21:38 +0300

    Update lecture 7 slides

--- Lecture7/code_examples/with_open.py
+++ Lecture7/code_examples/with_open.py
@@ -1,8 +1,8 @@
 f = open('my_file.txt', 'r') as f:
     # Content of my_file.txt: '1,0,2'
 values = f.read().split(',')
-# What happens
-result = int(values[0]) / int(values[1]) + int(values[2])
+# What happens?
+result = int(values[0]) / int(values[1])
 f.close()
 
 # Safer approach, file is closed
