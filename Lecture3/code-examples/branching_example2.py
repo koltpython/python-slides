1131f9694e8053d7abe88d17ac2a81582e5736d2
nmutlu98 <53997005+nmutlu98@users.noreply.github.com>
2020-02-11 10:34:50 +0300

    Changed branching example 2

--- Lecture3/code-examples/branching_example2.py
+++ Lecture3/code-examples/branching_example2.py
@@ -1,3 +1,14 @@
+operation = int(input())
+num1 = int(input())
+num2 = int(input())
+if operation == 1:
+    sum_two_numbers(num1,num2)
+elif operation == 2:
+    multiply_two_numbers(num1,num2)
+else:
+    divide_two_numbers(num1,num2)
+    
+"""
 if have_ideas_for_exciting_examples():
     .
     .
@@ -7,3 +18,4 @@ elif gul_sena_has_awesome_ideas():
 else:
     prepare_slides_about_your_misery()
 apologize_to_class()
+"""
