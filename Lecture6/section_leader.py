15717bc7e9b69f9aad40d6fa7bc4ca3e1ef18e1b
Ahmet Uysal <auysal16@ku.edu.tr>
2020-03-03 09:24:06 +0300

    Update lecture6 slides

---
+++ Lecture6/section_leader.py
@@ -0,0 +1,31 @@
+ceren = ['şeref', 'oya', 'ata', 'simge', 'deniz']
+haluk = ['matteo', 'hakan', 'tuan', 'mehmet', 'waseem']
+ahmet = ['kutay', 'mine', 'fatma', 'nergis']
+necla = ['göktuğ', 'beste', 'ilke']
+ayse = ['sedef', 'elif', 'şakir', 'zeynep', 'şahin']
+eralp = ['atahan', 'irem', 'ırmak', 'ahmet']
+hasan = ['uğur', 'ismail', 'murat', 'tuğçe']
+firat = ['ata', 'ömer', 'lara']
+sections = [ceren, haluk, ahmet, necla, ayse, eralp, hasan, firat]
+
+# number of unique names
+
+
+def find_section_leader_list(student_name):
+    pass
+
+
+section_dict = {
+    'ceren': ['şeref', 'oya', 'ata', 'simge', 'deniz'],
+    'haluk': ['matteo', 'hakan', 'tuan', 'mehmet', 'waseem'],
+    'ahmet': ['kutay', 'mine', 'fatma', 'nergis'],
+    'necla': ['göktuğ', 'beste', 'ilke'],
+    'ayse': ['sedef', 'elif', 'şakir', 'zeynep', 'şahin'],
+    'eralp': ['atahan', 'irem', 'ırmak', 'ahmet'],
+    'hasan': ['uğur', 'ismail', 'murat', 'tuğçe'],
+    'firat': ['ata', 'ömer', 'lara']
+}
+
+
+def find_section_leader_dict(section_info, student_name):
+    pass
