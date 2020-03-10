44382109703582744b121d6e9b419de98b89206f
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2020-03-10 17:30:03 +0300

    Fixed link in slides

--- Lecture7/lecture-examples/lecture7-1.py
+++ Lecture7/lecture-examples/lecture7-1.py
@@ -7,3 +7,17 @@ clues = ('rain', 'cake', 'glass', 'flower', 'napkin')
 # Store these words together in a dictionary, where the keys are the clues and the values are the words that the user tells.
 # We want another user to be able to learn the previous user's associations. For this reason, ask the user which word's association
 # they want to learn and print the related word.
+pairs = dict()
+
+for word in clues:
+    answer = input(f"What word comes to your mind when you see {word} ")
+    pairs[word] = answer
+
+print(pairs)
+
+key = input("What do you want to learn? ")
+while key != 'exit':
+    print(pairs[key])
+    key = input("What do you want to learn? ")
+
+open("texts/my_text.txt", mode='w')
--- Lecture7/lecture-examples/lecture7-2.py
+++ Lecture7/lecture-examples/lecture7-2.py
@@ -1,15 +1,35 @@
+try:
+    a = float(input("Performance of CPUA:"))
+    b = float(input("Performance of CPUA:"))
+    performance_rate = a/b
+except ZeroDivisionError:
+    print("The second input cannot be zero!")
+except ValueError as e:
+    print(e)
+else:
+    print(performance_rate)
 
-a = float(input("Performance of CPUA:"))
-b = float(input("Performance of CPUA:"))
-performance_rate = a/b
-
-print(f'I have been living here for {years} years')
+try:
+    print(f'I have been living here for {years} years')
+except NameError as e:
+    print(e)
 
 faculties = ["Case", "Ce", "Cs", "Ssch", "Med"]
 
-index = int(
-    input(f'These are the faculties {faculties} which index do you want to use?'))
-faculties[index]
-print(f'You chose {faculties[index]}')
+while True:
+    try:
+        index = int(
+            input(f'These are the faculties {faculties} which index do you want to use?'))
+        faculties[index]
+    except ValueError:
+        print("index should be an integer")
+    except IndexError as e:
+        print(e)
+    else:
+        print(f'You chose {faculties[index]}')
+        break
 
-print(2 + '2')
+try:
+    print(2 + '2')
+except TypeError as e:
+    print(e)
