92fa4fa6da0ed347741c28546bb3677fb38b6bed
ckocaogullar15 <ckocaogullar15@ku.edu.tr>
2020-03-10 13:22:39 +0300

    Added in-class exercises

---
+++ Lecture7/lecture-examples/lecture7-1.py
@@ -0,0 +1,9 @@
+
+# Free Association Game
+
+clues = ('rain', 'cake', 'glass', 'flower', 'napkin')
+
+# Let's play a game. Give the user these words one by one and ask them to give you the first word that comes to their mind.
+# Store these words together in a dictionary, where the keys are the clues and the values are the words that the user tells.
+# We want another user to be able to learn the previous user's associations. For this reason, ask the user which word's association
+# they want to learn and print the related word.
---
+++ Lecture7/lecture-examples/lecture7-2.py
@@ -0,0 +1,15 @@
+
+a = float(input("Performance of CPUA:"))
+b = float(input("Performance of CPUA:"))
+performance_rate = a/b
+
+print(f'I have been living here for {years} years')
+
+faculties = ["Case", "Ce", "Cs", "Ssch", "Med"]
+
+index = int(
+    input(f'These are the faculties {faculties} which index do you want to use?'))
+faculties[index]
+print(f'You chose {faculties[index]}')
+
+print(2 + '2')
